ad3d4cb7727af627030fbcf434efebbc5dd9a57e
Smith <issmith1@hilo.gateway.2wire.net>
2012-11-12 06:32:00 -0800

    initial ci

---
+++ vinspector/vehicle inspector/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  vehicle inspector
+//
+//  Created by Smith, Irene S. (ARC-TI)[Stinger Ghaffarian Technologies Inc. (SGT Inc.)] on 11/11/12.
+//  Copyright (c) 2012 Smith, Irene S. (ARC-TI)[Stinger Ghaffarian Technologies Inc. (SGT Inc.)]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ vinspector/vehicle inspector/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  vehicle inspector
+//
+//  Created by Smith, Irene S. (ARC-TI)[Stinger Ghaffarian Technologies Inc. (SGT Inc.)] on 11/11/12.
+//  Copyright (c) 2012 Smith, Irene S. (ARC-TI)[Stinger Ghaffarian Technologies Inc. (SGT Inc.)]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
